f2ee1f5b522178489d89cc99024ed302fc7e90ef
PARK Yong Seo <j@saro.me>
2022-09-24 19:39:48 +0900

    just const

--- src/main/kotlin/domain/conf/Vars.kt
+++ src/main/kotlin/domain/conf/Vars.kt
@@ -2,6 +2,6 @@ package domain.conf
 
 class Vars {
     companion object {
-        val CONF_FILE_NAME: String = "db-import.conf"
+        const val CONF_FILE_NAME: String = "db-import.conf"
     }
 }
